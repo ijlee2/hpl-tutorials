10af16029d4369921783c8e36397e2470884f2c8
ijlee2 <ijlee2@users.noreply.github.com>
2018-12-10 21:45:48 -0600

    Updated starter code for Tutorial 2

--- hpl-tutorial-02/app/components/my-list.js
+++ hpl-tutorial-02/app/components/my-list.js
@@ -6,16 +6,25 @@ export default Component.extend({
 
         this.set('people', [
             {
+                id: '0',
                 firstName: 'Isaac',
                 lastName: 'Lee',
+                joinDate: '08/13/2015',
+                isActive: true,
             },
             {
+                id: '1',
                 firstName: 'Fahad',
                 lastName: 'Memon',
+                joinDate: '12/06/2016',
+                isActive: false,
             },
             {
+                id: '2',
                 firstName: 'Ja-Andre',
                 lastName: 'Lamar',
+                joinDate: '07/21/2016',
+                isActive: true,
             },
         ]);
     },
