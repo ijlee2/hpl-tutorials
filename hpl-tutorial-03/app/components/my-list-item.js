92ae927f05d9f7ec11db69226f15c1831defa91d
ijlee2 <ijlee2@users.noreply.github.com>
2018-12-20 18:43:35 -0600

    Updated styling

--- hpl-tutorial-03/app/components/my-list-item.js
+++ hpl-tutorial-03/app/components/my-list-item.js
@@ -1,4 +1,23 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+    // For styling purposes (semantic HTML, too), we don't want Ember
+    // to surround the <li> tag with the <div> tag
+    tagName: '',
+
+    init() {
+        this._super(...arguments);
+
+        this.set('isEditing', false);
+    },
+
+    actions: {
+        editItem() {
+            this.set('isEditing', true);
+        },
+
+        saveItem() {
+            this.set('isEditing', false);
+        },
+    },
 });
