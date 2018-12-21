e5364df24a1289347f7fa48557831ae7c31fbc32
ijlee2 <ijlee2@users.noreply.github.com>
2018-12-20 19:25:19 -0600

    Added template linting

--- hpl-tutorial-03/.template-lintrc.js
+++ hpl-tutorial-03/.template-lintrc.js
@@ -1,5 +1,11 @@
 'use strict';
 
 module.exports = {
-  extends: 'recommended'
+    extends: 'recommended',
+    rules: {
+        'attribute-indentation': {
+            'indentation': 4,
+        },
+        'block-indentation': 4,
+    }
 };
